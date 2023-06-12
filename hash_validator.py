8334aa7a34a1f5f0213bedece08428c079cb6110
BorowiecM127 <masarnikmacius@gmail.com>
2023-06-12 21:56:11 +0200

    Initial commit

---
+++ hash_validator.py
@@ -0,0 +1,35 @@
+"""Script validating file SHA-256 hash with hash string"""
+import hashlib
+import argparse
+
+
+def calculate_sha256(file_path):
+    """Calculates the SHA-256 hash of a file."""
+    sha256_hash = hashlib.sha256()
+    with open(file_path, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
+
+
+def main():
+    """Main function"""
+    parser = argparse.ArgumentParser(
+        description="Calculate and compare SHA-256 hash for a file."
+    )
+
+    parser.add_argument("file_path", help="Path to the file")
+    parser.add_argument("hash_string", help="SHA-256 hash string")
+
+    args = parser.parse_args()
+
+    calculated_hash = calculate_sha256(args.file_path)
+
+    if calculated_hash == args.hash_string:
+        print("Hashes are equal!")
+    else:
+        print("Hashes are not equal!")
+
+
+if __name__ == "__main__":
+    main()
