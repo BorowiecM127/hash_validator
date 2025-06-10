ea5689749ebaff7919cbcfb545b6acd140824aa0
BorowiecM127 <masarnikmacius@gmail.com>
2025-06-10 20:36:47 +0200

    Refactor hash_validator.py for type hinting and improved readability

--- hash_validator.py
+++ hash_validator.py
@@ -1,30 +1,31 @@
 """Script validating file SHA-256 hash with hash string"""
 
-import hashlib
+from hashlib import sha256
 import argparse
+from _hashlib import HASH
 
 
 def calculate_sha256(file_path: str) -> str:
     """Calculates the SHA-256 hash of a file."""
-    sha256_hash = hashlib.sha256()
-    with open(file_path, "rb") as file:
+    sha256_hash: HASH = sha256()
+    with open(file=file_path, mode="rb") as file:
         for chunk in iter(lambda: file.read(4096), b""):
             sha256_hash.update(chunk)
     return sha256_hash.hexdigest()
 
 
-def main():
+def main() -> None:
     """Main function"""
-    parser = argparse.ArgumentParser(
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
         description="Calculate and compare SHA-256 hash for a file."
     )
 
     parser.add_argument("file_path", help="Path to the file")
     parser.add_argument("hash_string", help="SHA-256 hash string")
 
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
-    calculated_hash = calculate_sha256(args.file_path)
+    calculated_hash: str = calculate_sha256(file_path=args.file_path)
 
     print(f"calculated hash: {calculated_hash.lower()}")
     print(f"    hash_string: {args.hash_string.lower()}")
